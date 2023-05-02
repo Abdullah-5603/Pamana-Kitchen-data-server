a2e5a8fd13c58e34fc03cfb07a4b0b68f0b77b6a
Abu Abdullah <abdullah72308@gmail.com>
2023-05-02 23:32:35 +0600

    cuisine course data added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 const recipe = require('./Data/recipeData.json')
 const traditionalFood = require('./Data/traditionalFoodData.json')
+const cuisineCourse = require('./Data/cusineCourseData.json')
 
 app.use(cors());
 
@@ -12,14 +13,18 @@ app.get('/', (req, res) =>{
     res.send(recipe)
 })
 
-app.get('/traditionalFood', (req, res) =>{
-    res.send(traditionalFood);
-})
-
 app.get('/recipe/:id', (req, res) =>{
     const id = req.params.id;
     const selectedRecipe = recipe.find(rp => rp.id == id)
     res.send(selectedRecipe)
+})
+
+app.get('/traditionalFood', (req, res) =>{
+    res.send(traditionalFood);
+})
+
+app.get('/cuisineCourse', (req, res) =>{
+    res.send(cuisineCourse)
 })
 
 app.listen(port, ()=>{
